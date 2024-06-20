1b37679a981d44f2c6186d725b78789cb1c97173
Nabika Tomohiro <nabikatomohiro@NabikanoMacBook-Pro.local>
2024-06-20 12:22:46 +0900

    update for same query

--- src/GRMSD.cpp
+++ src/GRMSD.cpp
@@ -38,101 +38,184 @@ int main(int argc, char *argv[]){
 	string data_name = argv[3];
 	bool permit_mirror = atoi(argv[4]);
 	ofstream outputfile(argv[5]);
+	bool multiple_query = atoi(argv[6]);
     ifstream ifs_csv_file_model(model_name); 
     ifstream ifs_csv_file_data(data_name);
-	string line;
-	vector<MatrixXd> AA;
-	vector<vector<int> > pen_data;
-	while(getline(ifs_csv_file_data, line)) {
-		vector<double> strvec = split(line, ',');
-		int N_data = (int)strvec[0];
-		MatrixXd A = MatrixXd::Zero(3,N_data);
-	    vector<int> p(N_data);
-		for(int i=0;i<N_data;i++){
-			getline(ifs_csv_file_data, line);
+	if(multiple_query){
+		string line;
+		vector<MatrixXd> AA;
+		vector<vector<int> > pen_data;
+		while(getline(ifs_csv_file_data, line)) {
 			vector<double> strvec = split(line, ',');
-			A(0,i) = strvec[0];
-		    A(1,i) = strvec[1];
-		    A(2,i) = strvec[2];
-			p[i] = (int)strvec[3];
+			int N_data = (int)strvec[0];
+			MatrixXd A = MatrixXd::Zero(3,N_data);
+			vector<int> p(N_data);
+			for(int i=0;i<N_data;i++){
+				getline(ifs_csv_file_data, line);
+				vector<double> strvec = split(line, ',');
+				A(0,i) = strvec[0];
+				A(1,i) = strvec[1];
+				A(2,i) = strvec[2];
+				p[i] = (int)strvec[3];
+			}
+			AA.push_back(A);
+			pen_data.push_back(p);
 		}
-		AA.push_back(A);
-		pen_data.push_back(p);
-	}
 
-    vector<MatrixXd> BB;
-	vector<vector<int> > pen_model;
-	while(getline(ifs_csv_file_model, line)) {
-		vector<double> strvec = split(line, ',');
-		int N_model = (int)strvec[0];
-		MatrixXd B = MatrixXd::Zero(3,N_model);
-	    vector<int> p(N_model);
-		for(int i=0;i<N_model;i++){
-			getline(ifs_csv_file_model, line);
+		vector<MatrixXd> BB;
+		vector<vector<int> > pen_model;
+		while(getline(ifs_csv_file_model, line)) {
 			vector<double> strvec = split(line, ',');
-			B(0,i) = strvec[0];
-		    B(1,i) = strvec[1];
-		    B(2,i) = strvec[2];
-			p[i] = (int)strvec[3];
+			int N_model = (int)strvec[0];
+			MatrixXd B = MatrixXd::Zero(3,N_model);
+			vector<int> p(N_model);
+			for(int i=0;i<N_model;i++){
+				getline(ifs_csv_file_model, line);
+				vector<double> strvec = split(line, ',');
+				B(0,i) = strvec[0];
+				B(1,i) = strvec[1];
+				B(2,i) = strvec[2];
+				p[i] = (int)strvec[3];
+			}
+			BB.push_back(B);
+			pen_model.push_back(p);
 		}
-		BB.push_back(B);
-		pen_model.push_back(p);
-	}
-	vector<MatrixXd> Smentai;
-	Smentai.reserve(120);
-	for(int i=0;i<120;i++){
-		Smentai.push_back(MatrixXd::Zero(3,3));
-	}
-	int j = 0;
-	while(getline(ifs_csv_file, line)) {    
-		istringstream i_stream(line);
-		vector<double> strvec = split(line, ',');
-		for (int i=0; i<strvec.size()/3;i++){
-			Smentai[i](j,0) = strvec[3*i];
-			Smentai[i](j,1) = strvec[3*i+1];
-			Smentai[i](j,2) = strvec[3*i+2];
+		vector<MatrixXd> Smentai;
+		Smentai.reserve(120);
+		for(int i=0;i<120;i++){
+			Smentai.push_back(MatrixXd::Zero(3,3));
 		}
-		j = j + 1;
-	}
-
-	for(int i=0;i<AA.size();i++){
-		if(AA[i].size() == BB[i].size()){
-			if(function_name == "AO"){
-				Answer ans = AO_same_size(BB[i],AA[i],pen_model[i],pen_data[i],Smentai,permit_mirror);
-				output(ans,outputfile);
+		int j = 0;
+		while(getline(ifs_csv_file, line)) {    
+			istringstream i_stream(line);
+			vector<double> strvec = split(line, ',');
+			for (int i=0; i<strvec.size()/3;i++){
+				Smentai[i](j,0) = strvec[3*i];
+				Smentai[i](j,1) = strvec[3*i+1];
+				Smentai[i](j,2) = strvec[3*i+2];
 			}
-			if(function_name == "TSR"){
-				Answer ans = TSR_same_size(BB[i],AA[i],pen_model[i],pen_data[i],Smentai,permit_mirror,0.3);
-				output(ans,outputfile);
-			}
-			if(function_name == "IsometryOpt"){
-				Answer ans = IsometryOpt_same_size(BB[i],AA[i],pen_model[i],pen_data[i],permit_mirror,1,1);
-				output(ans,outputfile);
-			}
-			if(function_name == "MatchFastOpt"){
-				Answer ans = MatchFastOpt_same_size(BB[i],AA[i],pen_model[i],pen_data[i],permit_mirror);
-				output(ans,outputfile);
-			}
+			j = j + 1;
 		}
 
-		if(AA[i].size() != BB[i].size()){
-			if(function_name == "AO"){
-				Answer ans = AO_diff_size(BB[i],AA[i],pen_model[i],pen_data[i],Smentai,permit_mirror);
-				output(ans,outputfile);
+		for(int i=0;i<AA.size();i++){
+			if(AA[i].size() == BB[i].size()){
+				if(function_name == "AO"){
+					Answer ans = AO_same_size(BB[i],AA[i],pen_model[i],pen_data[i],Smentai,permit_mirror);
+					output(ans,outputfile);
+				}
+				if(function_name == "TSR"){
+					Answer ans = TSR_same_size(BB[i],AA[i],pen_model[i],pen_data[i],Smentai,permit_mirror,0.3);
+					output(ans,outputfile);
+				}
+				if(function_name == "IsometryOpt"){
+					Answer ans = IsometryOpt_same_size(BB[i],AA[i],pen_model[i],pen_data[i],permit_mirror,1,1);
+					output(ans,outputfile);
+				}
+				if(function_name == "MatchFastOpt"){
+					Answer ans = MatchFastOpt_same_size(BB[i],AA[i],pen_model[i],pen_data[i],permit_mirror);
+					output(ans,outputfile);
+				}
 			}
-			if(function_name == "TSR"){
-				cout << "TSR is not defined with molecules of different sizes" << endl;
-			}
-			if(function_name == "IsometryOpt"){
-				Answer ans = IsometryOpt_diff_size(BB[i],AA[i],pen_model[i],pen_data[i],permit_mirror,1,1);
-				output(ans,outputfile);
-			}
-			if(function_name == "MatchFastOpt"){
-				Answer ans = MatchFastOpt_diff_size(BB[i],AA[i],pen_model[i],pen_data[i],permit_mirror);
-				output(ans,outputfile);
+
+			if(AA[i].size() != BB[i].size()){
+				if(function_name == "AO"){
+					Answer ans = AO_diff_size(BB[i],AA[i],pen_model[i],pen_data[i],Smentai,permit_mirror);
+					output(ans,outputfile);
+				}
+				if(function_name == "TSR"){
+					cout << "TSR is not defined with molecules of different sizes" << endl;
+				}
+				if(function_name == "IsometryOpt"){
+					Answer ans = IsometryOpt_diff_size(BB[i],AA[i],pen_model[i],pen_data[i],permit_mirror,1,1);
+					output(ans,outputfile);
+				}
+				if(function_name == "MatchFastOpt"){
+					Answer ans = MatchFastOpt_diff_size(BB[i],AA[i],pen_model[i],pen_data[i],permit_mirror);
+					output(ans,outputfile);
+				}
 			}
 		}
+		outputfile.close();
+	}else{
+		string line;
+		vector<MatrixXd> Smentai;
+		Smentai.reserve(120);
+		for(int i=0;i<120;i++){
+			Smentai.push_back(MatrixXd::Zero(3,3));
+		}
+		int j = 0;
+		while(getline(ifs_csv_file, line)) {    
+			istringstream i_stream(line);
+			vector<double> strvec = split(line, ',');
+			for (int i=0; i<strvec.size()/3;i++){
+				Smentai[i](j,0) = strvec[3*i];
+				Smentai[i](j,1) = strvec[3*i+1];
+				Smentai[i](j,2) = strvec[3*i+2];
+			}
+			j = j + 1;
+		}while(getline(ifs_csv_file_model, line)) {
+            vector<double> strvec = split(line, ',');
+            int N_model = (int)strvec[0];
+            MatrixXd B = MatrixXd::Zero(3,N_model);
+            vector<int> pen_model(N_model);
+            for(int i=0;i<N_model;i++){
+                getline(ifs_csv_file_model, line);
+                vector<double> strvec = split(line, ',');
+                B(0,i) = strvec[0];
+                B(1,i) = strvec[1];
+                B(2,i) = strvec[2];
+                pen_model[i] = (int)strvec[3];
+            }
+            while(getline(ifs_csv_file_data, line)) {
+                vector<double> strvec = split(line, ',');
+                int N_data = (int)strvec[0];
+                MatrixXd A = MatrixXd::Zero(3,N_data);
+                vector<int> pen_data(N_data);
+                for(int i=0;i<N_data;i++){
+                    getline(ifs_csv_file_data, line);
+                    vector<double> strvec = split(line, ',');
+                    A(0,i) = strvec[0];
+                    A(1,i) = strvec[1];
+                    A(2,i) = strvec[2];
+                    pen_data[i] = (int)strvec[3];
+                }
+				if(A.size() == B.size()){
+					if(function_name == "AO"){
+						Answer ans = AO_same_size(B,A,pen_model,pen_data,Smentai,permit_mirror);
+						output(ans,outputfile);
+					}
+					if(function_name == "TSR"){
+						Answer ans = TSR_same_size(B,A,pen_model,pen_data,Smentai,permit_mirror,0.3);
+						output(ans,outputfile);
+					}
+					if(function_name == "IsometryOpt"){
+						Answer ans = IsometryOpt_same_size(B,A,pen_model,pen_data,permit_mirror,1,1);
+						output(ans,outputfile);
+					}
+					if(function_name == "MatchFastOpt"){
+						Answer ans = MatchFastOpt_same_size(B,A,pen_model,pen_data,permit_mirror);
+						output(ans,outputfile);
+					}
+				}else{
+					if(function_name == "AO"){
+						Answer ans = AO_diff_size(B,A,pen_model,pen_data,Smentai,permit_mirror);
+						output(ans,outputfile);
+					}
+					if(function_name == "TSR"){
+						cout << "TSR is not defined with molecules of different sizes" << endl;
+					}
+					if(function_name == "IsometryOpt"){
+						Answer ans = IsometryOpt_diff_size(B,A,pen_model,pen_data,permit_mirror,1,1);
+						output(ans,outputfile);
+					}
+					if(function_name == "MatchFastOpt"){
+						Answer ans = MatchFastOpt_diff_size(B,A,pen_model,pen_data,permit_mirror);
+						output(ans,outputfile);
+					}
+				}
+			}
+		}
+		outputfile.close();
 	}
-	outputfile.close();
     return 0;
 }
--- src/Substructure.cpp
+++ src/Substructure.cpp
@@ -44,68 +44,111 @@ int main(int argc, char *argv[]){
 	bool permit_mirror = atoi(argv[4]);
     double r = stod(argv[5]);
     ofstream outputfile(argv[6]);
-	string line;
-	vector<MatrixXd> AA;
-	vector<vector<int> > pen_data;
-	while(getline(ifs_csv_file_data, line)) {
-		vector<double> strvec = split(line, ',');
-		int N_data = (int)strvec[0];
-		MatrixXd A = MatrixXd::Zero(3,N_data);
-	    vector<int> p(N_data);
-		for(int i=0;i<N_data;i++){
-			getline(ifs_csv_file_data, line);
-			vector<double> strvec = split(line, ',');
-			A(0,i) = strvec[0];
-		    A(1,i) = strvec[1];
-		    A(2,i) = strvec[2];
-			p[i] = (int)strvec[3];
-		}
-		AA.push_back(A);
-		pen_data.push_back(p);
-	}
-
-    vector<MatrixXd> BB;
-	vector<vector<int> > pen_model;
-	while(getline(ifs_csv_file_model, line)) {
-		vector<double> strvec = split(line, ',');
-		int N_model = (int)strvec[0];
-		MatrixXd B = MatrixXd::Zero(3,N_model);
-	    vector<int> p(N_model);
-		for(int i=0;i<N_model;i++){
-			getline(ifs_csv_file_model, line);
-			vector<double> strvec = split(line, ',');
-			B(0,i) = strvec[0];
-		    B(1,i) = strvec[1];
-		    B(2,i) = strvec[2];
-			p[i] = (int)strvec[3];
-		}
-		BB.push_back(B);
-		pen_model.push_back(p);
-	}
-
-    if(function_name == "IsometrySearch"){
-        for(int i=0;i<AA.size();i++){
-            vector<Answer> ans_multiple = IsometrySearch(BB[i],AA[i],pen_model[i],pen_data[i],r,permit_mirror);
-            output(ans_multiple,outputfile);
+    bool multiple_query = atoi(argv[7]);
+    if(multiple_query){
+        string line;
+        vector<MatrixXd> AA;
+        vector<vector<int> > pen_data;
+        while(getline(ifs_csv_file_data, line)) {
+            vector<double> strvec = split(line, ',');
+            int N_data = (int)strvec[0];
+            MatrixXd A = MatrixXd::Zero(3,N_data);
+            vector<int> p(N_data);
+            for(int i=0;i<N_data;i++){
+                getline(ifs_csv_file_data, line);
+                vector<double> strvec = split(line, ',');
+                A(0,i) = strvec[0];
+                A(1,i) = strvec[1];
+                A(2,i) = strvec[2];
+                p[i] = (int)strvec[3];
+            }
+            AA.push_back(A);
+            pen_data.push_back(p);
         }
-    }
 
-    if(function_name == "MatchFPT"){
-        for(int i=0;i<AA.size();i++){
-            vector<Answer> ans_multiple = MatchFPT(BB[i],AA[i],pen_model[i],pen_data[i],r,permit_mirror);
-            output(ans_multiple,outputfile);
+        vector<MatrixXd> BB;
+        vector<vector<int> > pen_model;
+        while(getline(ifs_csv_file_model, line)) {
+            vector<double> strvec = split(line, ',');
+            int N_model = (int)strvec[0];
+            MatrixXd B = MatrixXd::Zero(3,N_model);
+            vector<int> p(N_model);
+            for(int i=0;i<N_model;i++){
+                getline(ifs_csv_file_model, line);
+                vector<double> strvec = split(line, ',');
+                B(0,i) = strvec[0];
+                B(1,i) = strvec[1];
+                B(2,i) = strvec[2];
+                p[i] = (int)strvec[3];
+            }
+            BB.push_back(B);
+            pen_model.push_back(p);
         }
-    }
 
-    if(function_name == "Improved-MatchFPT"){
-        for(int i=0;i<AA.size();i++){
-            vector<Answer> ans_multiple = Improved_MatchFPT(BB[i],AA[i],pen_model[i],pen_data[i],r,permit_mirror);
-            output(ans_multiple,outputfile);
+        if(function_name == "IsometrySearch"){
+            for(int i=0;i<AA.size();i++){
+                vector<Answer> ans_multiple = IsometrySearch(BB[i],AA[i],pen_model[i],pen_data[i],r,permit_mirror);
+                output(ans_multiple,outputfile);
+            }
         }
+
+        if(function_name == "MatchFPT"){
+            for(int i=0;i<AA.size();i++){
+                vector<Answer> ans_multiple = MatchFPT(BB[i],AA[i],pen_model[i],pen_data[i],r,permit_mirror);
+                output(ans_multiple,outputfile);
+            }
+        }
+
+        if(function_name == "Improved-MatchFPT"){
+            for(int i=0;i<AA.size();i++){
+                vector<Answer> ans_multiple = Improved_MatchFPT(BB[i],AA[i],pen_model[i],pen_data[i],r,permit_mirror);
+                output(ans_multiple,outputfile);
+            }
+        }
+        outputfile.close();
+    }else{
+        string line;
+        while(getline(ifs_csv_file_model, line)) {
+            vector<double> strvec = split(line, ',');
+            int N_model = (int)strvec[0];
+            MatrixXd B = MatrixXd::Zero(3,N_model);
+            vector<int> pen_model(N_model);
+            for(int i=0;i<N_model;i++){
+                getline(ifs_csv_file_model, line);
+                vector<double> strvec = split(line, ',');
+                B(0,i) = strvec[0];
+                B(1,i) = strvec[1];
+                B(2,i) = strvec[2];
+                pen_model[i] = (int)strvec[3];
+            }
+            while(getline(ifs_csv_file_data, line)) {
+                vector<double> strvec = split(line, ',');
+                int N_data = (int)strvec[0];
+                MatrixXd A = MatrixXd::Zero(3,N_data);
+                vector<int> pen_data(N_data);
+                for(int i=0;i<N_data;i++){
+                    getline(ifs_csv_file_data, line);
+                    vector<double> strvec = split(line, ',');
+                    A(0,i) = strvec[0];
+                    A(1,i) = strvec[1];
+                    A(2,i) = strvec[2];
+                    pen_data[i] = (int)strvec[3];
+                }
+                if(function_name == "IsometrySearch"){
+                    vector<Answer> ans = IsometrySearch(B,A,pen_model,pen_data,r,permit_mirror);
+                    output(ans,outputfile);
+                }else if(function_name == "MatchFPT"){
+                    vector<Answer> ans = MatchFPT(B,A,pen_model,pen_data,r,permit_mirror);
+                    output(ans,outputfile);
+                }else if(function_name == "Improved-MatchFPT"){
+                    vector<Answer> ans = Improved_MatchFPT(B,A,pen_model,pen_data,r,permit_mirror);
+                    output(ans,outputfile);
+                }
+            }
+        }
+        outputfile.close();
     }
 
 	
-	
-	outputfile.close();
     return 0;
 }
